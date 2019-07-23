90f5e6fab5efda7966b29b1cb57236738c01aba0
Andrea Francis <soriajimenezandrea@hotmail.com>
2019-07-22 22:11:26 -0400

    Ordering files

--- sistema/frontend/src/app/app.routes.ts
+++ sistema/frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import {HomeComponent} from "./pages/home/home.component";
 
 export const ROUTES: Routes = [
     // routes from pages
-    {path: 'home', component: HomeComponent, data: {title: 'Anamnesis'}},
+    {path: 'home', component: HomeComponent, data: {title: 'Encuesta TICs'}},
 
     // default redirect
     {path: '**', redirectTo: '/home'}
